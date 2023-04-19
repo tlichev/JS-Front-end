1c87f6798b137ec1aaad6ef8a4b7f3a70893fceb
trayanlichev <96567061+trayanlichev@users.noreply.github.com>
2023-04-20 01:28:35 +0300

    Add files via upload

---
+++ 1.JS Syntax Fundamentals/numbersFromNtoM.js
@@ -0,0 +1,7 @@
+function slove(m,n){
+    for (let index = m; index >=n; index--) {
+        console.log(index)
+        
+    }
+}
+slove(6,2)
---
+++ 1.JS Syntax Fundamentals/substring.js
@@ -0,0 +1,8 @@
+function solve(text, startIndex, count){
+    let res;
+    res = text.substr(startIndex, count)
+    console.log(res)
+}
+
+solve('ASentence', 1, 8)
+solve('SkipWord', 4, 7)
